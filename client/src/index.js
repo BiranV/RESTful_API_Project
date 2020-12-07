2a044f5c8af0c51c9aa610f06fca58a66e740a02
biran varon <biraniv@gmail.com>
2020-12-07 22:45:58 +0200

    update

---
+++ client/src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App'; 
+
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+
+const iniState = {
+  siteName: "Biran Project App"
+}
+
+const reducer = (state = iniState,action) => {
+  
+      switch(action.type){
+          case "CHANGE_SITE_NAME":
+            state = {...state, siteName: action.payload}
+          break;
+      }
+    return state;
+}
+
+const store  = createStore(reducer)
+
+ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  ,document.getElementById('root')
+);
